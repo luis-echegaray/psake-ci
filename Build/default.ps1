5ed100a784f770d0e8a88beae3a0b43b8956227b
Luis Echegaray <mrluisechegaray@gmail.com>
2015-10-12 16:30:05 -0400

    added test projects (default projects from ASP.NET)

--- Build/default.ps1
+++ Build/default.ps1
@@ -2,9 +2,16 @@
 	$testMessage = 'Executed Test!'
 	$compileMessage = 'Executed Compile!'
 	$cleanMessage = 'Executed Clean!'
+
+	$outputDirectory = "..\.build"
 }
 
 task default -depends Test
+
+task Init -description "Initialises the build by removing previous artifacts and creating output directories" {
+	Write-Host "Creating output directory located at ..\.build"
+	New-Item "..\.build" -ItemType Directory
+}
 
 task Clean -description "Remove temporary files" {
 	Write-Host $cleanMessage
