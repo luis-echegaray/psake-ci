eb2844d147bd1b44d53443f3aa5c9b1adddbd64f
Luis Echegaray <mrluisechegaray@gmail.com>
2015-10-12 16:20:28 -0400

    initial commit, first build script skeleton

---
+++ Build/build.ps1
@@ -0,0 +1,11 @@
+﻿cls
+
+#'[p]sake' is the same as 'psake' but $Error is not polluted
+Remove-Module [p]sake
+
+# find psake's path
+$psakeModule = (Get-ChildItem ("..\packages\psake.4.4.2\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1
+
+Import-Module $psakeModule
+
+Invoke-psake -buildFile .\default.ps1 -taskList Test -properties @{ "testMessage"="What am I doing?"}
---
+++ Build/default.ps1
@@ -0,0 +1,19 @@
+﻿properties {
+	$testMessage = 'Executed Test!'
+	$compileMessage = 'Executed Compile!'
+	$cleanMessage = 'Executed Clean!'
+}
+
+task default -depends Test
+
+task Clean -description "Remove temporary files" {
+	Write-Host $cleanMessage
+}
+
+task Compile -depends Clean -description "Compile the code" {
+	Write-Host $compileMessage
+}
+
+task Test -depends Compile, Clean -description "Run unit tests" {
+	Write-Host $testMessage
+}
